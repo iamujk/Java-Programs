f166ff999db0801b6242084724b196281325e967
iamujk <kumar30ujjwal@gmail.com>
2026-05-05 23:11:12 +0530

    New file

--- DecodeString.java
+++ DecodeString.java
@@ -1,25 +1,31 @@
 public class DecodeString {
     public static void main(String[] args) {
-        String input = "A1B2CD1AI3C0R2";
+        String input = "A1B2CD1AI10C0R2";
         StringBuilder result = new StringBuilder();
-        StringBuilder temp = new StringBuilder();
-
-        for (int i = 0; i < input.length(); i++) {
+        StringBuilder group = new StringBuilder();
+        int i = 0;
+        while (i < input.length()) {
             char ch = input.charAt(i);
-
+            // 1. Build letter group
             if (Character.isLetter(ch)) {
-                temp.append(ch); // build group
-            } else if (Character.isDigit(ch)) {
-                int count = ch - '0';  //converts char into int
-
-                for (int j = 0; j < count; j++) {
-                    result.append(temp);
+                group.append(ch);
+                i++;
+            }
+            // 2. When digit found, process full number
+            else if (Character.isDigit(ch)) {
+                int num = 0;
+                while (i < input.length() && Character.isDigit(input.charAt(i))) {
+                    num = num * 10 + (input.charAt(i) - '0');
+                    i++;
                 }
-
-                temp.setLength(0); // reset group
+                // repeat group
+                for (int j = 0; j < num; j++) {
+                    result.append(group);
+                }
+                // reset group
+                group.setLength(0);
             }
         }
-
-        System.out.println(result.toString());
+        System.out.println(result);
     }
 }
